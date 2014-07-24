35889145d5e214c5bb665b56023314f386bd8be7
pavel <pavelavw@yahoo.com>
2014-07-24 17:36:11 +0600

    some desparate code changes to try fix heroku

--- Gemfile
+++ Gemfile
@@ -3,7 +3,6 @@ source 'https://rubygems.org'
 ruby '1.9.3'
 gem 'rails', '4.1.1'
 # Rails defaults
-gem 'sqlite3'
 gem 'sass-rails', '~> 4.0.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
@@ -22,9 +21,14 @@ gem 'google_drive'
 gem 'high_voltage'
 gem 'simple_form'
 gem 'zurb-foundation'
-
 group :development do
-	gem 'better_errors'
-	gem 'quiet_assets'
-	gem 'rails_layout'
+gem 'better_errors'
+gem 'quiet_assets'
+gem 'rails_layout'
+gem 'sqlite3'
+end
+group :production do
+gem 'pg'
+gem 'rails_12factor'
+gem 'thin'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -80,4 +80,19 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-end
+  config.action_mailer.default_url_options = { :host => ENV["DOMAIN_NAME"] }
+	config.action_mailer.delivery_method = :smtp
+	config.action_mailer.perform_deliveries = true
+	config.action_mailer.raise_delivery_errors = false
+	config.action_mailer.default :charset => "utf-8"
+	config.action_mailer.smtp_settings = {
+					address: "smtp.gmail.com",
+					port: 587,
+					domain: ENV["DOMAIN_NAME"],
+					authentication: "plain",
+					enable_starttls_auto: true,
+					user_name: ENV["GMAIL_USERNAME"],
+					password: ENV["GMAIL_PASSWORD"]
+					}
+	  
+   end
